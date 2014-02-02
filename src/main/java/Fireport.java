e67dd0b1e56ecb2a05f74192f071e0068ad4322a
vskarine <skarine@gmail.com>
2014-02-02 02:33:05 -0800

    Basic Firebase test

--- src/main/java/Fireport.java
+++ src/main/java/Fireport.java
@@ -1,3 +1,8 @@
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 /**
  * User: vassili
  * Date: 2/2/14
@@ -6,5 +11,26 @@
  */
 public class Fireport {
   public static void main(String[]args) {
+    // Create a reference to a Firebase location
+    Firebase ref = new Firebase("https://fireport.firebaseio.com/");
+
+    // Write data to Firebase
+    ref.setValue("Testing firebase!");
+
+    // Read data and react to changes
+    ref.addValueEventListener(new ValueEventListener() {
+
+      @Override
+      public void onDataChange(DataSnapshot snap) {
+        System.out.println(snap.getName() + " -> " + snap.getValue());
+      }
+
+      @Override
+      public void onCancelled(FirebaseError firebaseError) {}
+    });
+
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e) {}
   }
 }
