1b10a41fa8ae8f7bfb042c5274c7a17f9e117316
vassili <vassili@addepar.com>
2014-02-02 02:15:55 -0800

    Initial commit

---
+++ src/main/java/Fireport.java
@@ -0,0 +1,10 @@
+/**
+ * User: vassili
+ * Date: 2/2/14
+ * Time: 2:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Fireport {
+  public static void main(String[]args) {
+  }
+}
